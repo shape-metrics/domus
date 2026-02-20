920917a325b662aa6d5d3a72c5c0fbae01939fe5
friscobuffo <gio.andreola@stud.uniroma3.it>
2026-02-20 15:10:26 +0100

    Config update removed throws

--- include/domus/config/config.hpp
+++ include/domus/config/config.hpp
@@ -1,18 +1,40 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#include <memory>
+#include <expected>
+#include <filesystem>
+#include <fstream>
+#include <optional>
 #include <string>
-
-class ConfigImpl;
+#include <unordered_map>
 
 class Config {
-    std::unique_ptr<ConfigImpl> m_config_impl;
+    std::unordered_map<std::string, std::string> m_config_map;
+    Config();
+    void add_key_value(std::string key, std::string value) { m_config_map[key] = value; }
 
   public:
-    explicit Config(const std::string& filename);
-    [[nodiscard]] const std::string& get(const std::string& key) const;
-    ~Config();
+    std::optional<std::string> get(std::string key) const {
+        if (!m_config_map.contains(key))
+            return std::nullopt;
+        return m_config_map.at(key);
+    }
+    static std::expected<Config, std::string> create(std::filesystem::path path) {
+        std::ifstream file(path);
+        if (!file.is_open())
+            return std::unexpected("Cannot open config file at" + path.string());
+        std::string line;
+        Config config;
+        while (std::getline(file, line)) {
+            auto pos = line.find('=');
+            if (pos != std::string::npos) {
+                auto key = line.substr(0, pos);
+                auto value = line.substr(pos + 1);
+                config.add_key_value(key, value);
+            }
+        }
+        return std::move(config);
+    }
 };
 
 #endif
